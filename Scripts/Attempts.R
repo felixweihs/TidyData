c8d6bcb3135ef3b37b1d6b1c3228181c2642ed17
Weihs <wei115@csiro.au>
2019-10-10 09:39:27 +1100

    This script is for testing blocks which did not make into the final script

---
+++ Scripts/Attempts.R
@@ -0,0 +1,27 @@
+#This script contains all code that I experimented with but didn't make it to the final version
+#Variable names here are NOT compatible with each other
+
+#Read in individual files
+DataSetA <- read_csv("Data/DataSetA.csv")
+DataSetB <- read_csv("Data/DataSetB.csv")
+DataSetC <- read_csv("Data/DataSetC.csv")
+DataSetD <- read_csv("Data/DataSetD.csv")
+
+#Tidy Individual DataSet
+DataSetA_tidy <- DataSetA %>% 
+  slice(8:2000) %>% #Remove annotations that were automatically created by the CYBERTONGUE
+  select(1:3) %>% #Remove ratios column -> Inaccurate values
+  rename(time = 1, green = 2, blue = 3) %>% #Rename columns into sensible headers
+  mutate(BRET_ratio = as.numeric(green) / as.numeric(blue)) #Calculate and add column with BRET ratios
+DataSetB_tidy <- DataSetB %>% 
+  slice(8:2000) %>% #Remove annotations that were automatically created by the CYBERTONGUE
+  select(1:3) %>% #Remove ratios column -> Inaccurate values
+  rename(time = 1, green = 2, blue = 3) %>% #Rename columns into sensible headers
+  mutate(BRET_ratio = as.numeric(green) / as.numeric(blue)) #Calculate and add column with BRET ratios
+
+#5 min BRET ratio of individual dataframes
+DataFrameA_analysis <- DataFrameA_tidy %>% 
+  select(time, BRET_ratio) %>% 
+  filter(row_number() >= 600) %>% 
+  filter(row_number() <= 41) %>% 
+  summarise(mean(BRET_ratio))
