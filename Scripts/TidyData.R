31c610ea8ea2670126daaa4d22b11a83267e8adf
Weihs <wei115@csiro.au>
2019-11-14 14:04:25 +1100

    Extended analysis to 12 files, added data analysis (graph+table data)

--- Scripts/TidyData.R
+++ Scripts/TidyData.R
@@ -22,8 +22,11 @@ analysis_6 <- subset(AllData_tidy, Time >=360 & Time <= 380) %>%
   summarise(six_min = mean(Ratio))
 analysis_7 <- subset(AllData_tidy, Time >=420 & Time <= 440) %>% 
   summarise(seven_min = mean(Ratio))
+analysis_8 <- subset(AllData_tidy, Time >=480 & Time <= 500) %>% 
+  summarise(eight_min = mean(Ratio))
 analysis <- full_join(analysis_5, analysis_6)
 analysis <- full_join(analysis, analysis_7)
+analysis <- full_join(analysis, analysis_8)
 
 #Mutate datasets into groups
 analysis_summary <- analysis %>% 
@@ -36,27 +39,54 @@ analysis_summary <- analysis %>%
       dataset == "6" ~ "2",
       dataset == "7" ~ "3",
       dataset == "8" ~ "3",
-      dataset == "9" ~ "3")) %>% 
+      dataset == "9" ~ "3",
+      dataset == "10" ~ "4",
+      dataset == "11" ~ "4",
+      dataset == "12" ~ "4")) %>% 
   group_by(dataset)
 
 ## Putting BRET ratios together and calculate means and SD
 analysis_summary5 <- summarise(analysis_summary, BRET_ratio = mean(five_min), SD = sd(five_min)) %>% add_column(minutes = 5)
 analysis_summary6 <- summarise(analysis_summary, BRET_ratio = mean(six_min), SD = sd(six_min)) %>% add_column(minutes = 6)
 analysis_summary7 <- summarise(analysis_summary, BRET_ratio = mean(seven_min), SD = sd(seven_min)) %>% add_column(minutes = 7)
+analysis_summary8 <- summarise(analysis_summary, BRET_ratio = mean(eight_min), SD = sd(eight_min)) %>% add_column(minutes = 8)
 
 BRETratio_summary <- full_join(analysis_summary5, analysis_summary6)
 BRETratio_summary <- full_join(BRETratio_summary, analysis_summary7)
+BRETratio_summary <- full_join(BRETratio_summary, analysis_summary8)
 
 ## Plotting - Means and error bar
 ##
-graph_analysis <- ggplot(BRETratio_summary, aes(x=minutes, y=BRET_ratio, fill = dataset)) + 
-  geom_bar(stat="identity", color="black", position=position_dodge()) +
-  geom_errorbar(aes(ymin=BRET_ratio, ymax=BRET_ratio+SD), width=0.2,
-                position=position_dodge(.9)) +
-  labs(title="BRET Ratio comparison", x="Incubation time [min]", y = "BRET Ratio") +
-  theme_bw() +
-  scale_fill_manual(values=c('#999999','#E69F00'))
 
+
+analysis_summary2 <- analysis_summary %>% 
+  gather(group, value, -dataset) %>% 
+  mutate(group = case_when(
+    group == "five_min" ~ "5",
+    group == "six_min" ~ "6",
+    group == "seven_min" ~ "7",
+    group == "eight_min" ~ "8")) %>% 
+  group_by(dataset, group)
+
+rm(Stats1, Stats2)
+Stats1 <- summarise(analysis_summary2, mean = mean(value, na.rm=TRUE), SD = sd(value, na.rm=TRUE))
+Stats2 <-  analysis_summary2 %>% summarise_each(funs(sum(!is.na(.))))
+Stats_table <- full_join(Stats1, Stats2) %>% rename('incubation (min)' = group, 'n' = value) %>% ungroup()
+
+
+analysis_summary2_lm <- lm(value ~ as.numeric(group), data = analysis_summary2)
+
+graph_analysis2 <- ggplot(analysis_summary2, aes(x = as.numeric(group), y = value, colour = dataset, group = dataset)) +
+          geom_point(alpha = 0.5) +
+          stat_smooth(method = "lm") + ylim(0,1) +
+          labs(x = "Incubation time (min)", y = "BRET ratio") +
+          theme(axis.title.x = element_text(size = 14, face = "bold"),
+                axis.title.y = element_text(size = 14, face = "bold"),
+                legend.title = element_text(size = 20, face = "bold"),
+                legend.text = element_text(size = 12, face = "bold"),
+                axis.text = element_text(size = 14, face = "bold"),
+                legend.key = element_rect(fill = "white"))
+graph_analysis2
 #Tidy bioluminescence and ratio data
 AllData_tidy_signal <- AllData_tidy %>% 
   select(1:4) %>% 
@@ -65,6 +95,8 @@ AllData_tidy_signal <- AllData_tidy %>%
 AllData_tidy_ratio <- AllData_tidy %>% 
   select(1,2,5) %>% 
   gather(colour, Ratio, -dataset, -Time)
+
+
 
 # Produce graphs with bioluminescence data as a function of time
 graph_plot_signal <- ggplot(AllData_tidy_signal, 
@@ -75,36 +107,34 @@ graph_plot_signal <- ggplot(AllData_tidy_signal,
   theme_light() +
   theme(legend.position = "none",
         axis.title.x = element_blank(),
-        axis.text.x = element_blank()) +
+        axis.text.x = element_blank(),
+        strip.text = element_text(face="bold", size=10, colour = "black"),
+        strip.background = element_rect(fill="white", colour="black",size=2)) +
   ylim(0, 30000) +
   xlim(0, 600) +
-  facet_wrap(~dataset)
+  facet_wrap(~dataset, nrow = 1)
 
 # Produce graphs with BRET ratio data as a function of time
 graph_plot_ratio <- ggplot(AllData_tidy_ratio, 
                             aes(x=as.numeric(Time), y=as.numeric(Ratio))) + 
   geom_point() +
-  labs(title="", x="time [sec]", y = "BRET Ratio (Green/Blue") +
+  labs(title="", x="time [sec]", y = "BRET Ratio (Green/Blue)") +
   scale_colour_manual(values = c("black")) +
   theme_light() +
   theme(legend.position = "none",
-        axis.title.x = element_blank(),
-        axis.text.x = element_blank()) +
+        strip.text = element_text(face="bold", size=10, colour = "black"),
+        strip.background = element_rect(fill="white", colour="black",size=2)) +
   ylim(0, 1) +
   xlim(0, 600) +
-  facet_wrap(~dataset)
+  facet_wrap(~dataset, nrow = 1)
 
 # Connect signal and ratio graphs
-middlerow_plot <- plot_grid(Graph_A_1, Graph_B_1, Graph_C_1, Graph_D_1, Graph_E_1, Graph_F_1, 
-                            labels = c("A", "B","C", "D","E", "F"),
-                            nrow = 1,
-                            rel_widths = c(1.4,1,1,1,1,1))
-bottomrow_plot <- plot_grid(Graph_A_2, Graph_B_2, Graph_C_2, Graph_D_2, Graph_E_2, Graph_F_2, 
-                            nrow = 1,
-                            rel_widths = c(1.4,1,1,1,1,1))
-graph_plot_raw <- plot_grid(middlerow_plot, bottomrow_plot, 
-                            ncol = 1,
+graph_plot_raw <- plot_grid(graph_plot_signal, graph_plot_ratio, 
+                            ncol = 1, nrow = 2,
                             rel_heights = c(1,1))
+graph_plot_raw
+
+
 
 
 
@@ -125,7 +155,9 @@ graph_plot_signal2 <- ggplot(
   ylim(0, 25000) +
   xlim(0, 600) +
   theme(axis.title.x = element_blank(),
-        axis.text.x = element_blank())
+        axis.text.x = element_blank(),
+        text = element_text(size=60),
+        axis.text.y = element_text(size=50))
 graph_plot_signal2
 
 graph_plot_ratio2 <- ggplot(
@@ -134,7 +166,10 @@ graph_plot_ratio2 <- ggplot(
   geom_line(size = 2) +
   scale_colour_manual(values = c("black")) +
   labs(x = "time (sec)", y = "BRET ratio") +
-  theme(legend.position = "bottom") +
+  theme(legend.position = "bottom",
+        text = element_text(size=60),
+        axis.text.x = element_text(size=50),
+        axis.text.y = element_text(size=50)) +
   ylim(0, 1) +
   xlim(0, 600)
 graph_plot_ratio2
@@ -150,6 +185,20 @@ newanime_test2 <- animate(newanime2, height = 800, width =1600, fps = 5)
 anim_save("Results/signal_anime.gif", animation = newanime_test)
 anim_save("Results/ratio_anime.gif", animation = newanime_test2)
 
-# Save files
+#Save files
 ggsave("Results/raw_data.png", plot = graph_plot_raw, height = 10, width = 14)
 ggsave("Results/toprow_plot.png", plot = toprow_plot, height = 8, width = 14)
+
+#6 visualisations with the entire data
+AllData_tidy_actual <- AllData_tidy %>% 
+  gather(colour, Signal, -dataset, -Time) %>% 
+  group_by(dataset)
+
+#Outdated results graph
+graph_analysis <- ggplot(BRETratio_summary, aes(x=minutes, y=BRET_ratio, fill = dataset)) + 
+  geom_bar(stat="identity", color="black", position=position_dodge()) +
+  geom_errorbar(aes(ymin=BRET_ratio, ymax=BRET_ratio+SD), width=0.2,
+                position=position_dodge(.9)) +
+  labs(title="BRET Ratio comparison", x="Incubation time [min]", y = "BRET Ratio") +
+  theme_bw() +
+  scale_fill_manual(values=c('#999999','#E69F00'))
